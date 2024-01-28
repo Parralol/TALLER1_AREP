b090a7617768505e38f708d209323ebe57da2247
Parralol <santinico36@hotmail.com>
2024-01-27 19:18:20 -0500

    funcional final

--- miprimera-app/src/main/java/edu/escuelaing/arem/ASE/app/HttpServer.java
+++ miprimera-app/src/main/java/edu/escuelaing/arem/ASE/app/HttpServer.java
@@ -1,13 +1,17 @@
 package edu.escuelaing.arem.ASE.app;
 
 import java.net.*;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 import java.io.*; 
 
 public class HttpServer {
-    static String key = "&apikey=b5ed8d05";
-    static String url = "http://www.omdbapi.com/?t=";
+    private static String key = "&apikey=b5ed8d05";
+    private static String url = "http://www.omdbapi.com/?t=";
+    private static Map<String, String> cache = new HashMap<String, String>();
     public static void main(String[] args) throws IOException {
 
         boolean hasprint = false;
@@ -36,13 +40,13 @@ public class HttpServer {
             String inputLine, outputLine;
             int count = 0;
             String request="";
-
+            String path = "";
             while ((inputLine = in.readLine()) != null) {
                 if(count == 0){
                     request = inputLine;
+                    path = getPath(request);
                     request = getQuery(request);
-                    System.out.println(request);
-
+                    
                     count +=1;
                 }
                 
@@ -52,65 +56,69 @@ public class HttpServer {
                 }
             }
             out.println("");
-            if(!hasprint){
-            outputLine = "HTTP/1.1 200 OK"
-                    + "Content-Type:text/html; charset=ISO-8859-1\r\n"
-                    + "\r\n"
-                    + "<!DOCTYPE html>\r\n" + //
-                    "<html>\r\n" + //
-                    "    <head>\r\n" + //
-                    "        <title>Form Example</title>\r\n" + //
-                    "        <meta charset=\"UTF-8\">\r\n" + //
-                    "        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n" + //
-                    "    </head>\r\n" + //
-                    "    <body>\r\n" + //
-                    "        <h1>Form with GET</h1>\r\n" + //
-                    "        <form action=\"/hello\">\r\n" + //
-                    "            <label for=\"name\">Name:</label><br>\r\n" + //
-                    "            <input type=\"text\" id=\"name\" name=\"name\" value=\"John\"><br><br>\r\n" + //
-                    "            <input type=\"button\" value=\"Submit\" onclick=\"loadGetMsg()\">\r\n" + //
-                    "        </form> \r\n" + //
-                    "        <div id=\"getrespmsg\"></div>\r\n" + //
-                    "\r\n" + //
-                    "        <script>\r\n" + //
-                    "            function loadGetMsg() {\r\n" + //
-                    "                let nameVar = document.getElementById(\"name\").value;\r\n" + //
-                    "                const xhttp = new XMLHttpRequest();\r\n" + //
-                    "                xhttp.onload = function() {\r\n" + //
-                    "                    document.getElementById(\"getrespmsg\").innerHTML =\r\n" + //
-                    "                    this.responseText;\r\n" + //
-                    "                }\r\n" + //
-                    "                xhttp.open(\"GET\", \"/hello?name=\"+nameVar);\r\n" + //
-                    "                xhttp.send();\r\n" + //
-                    "            }\r\n" + //
-                    "        </script>\r\n" + //
-                    "\r\n" + //
-                    "        <h1>Form with POST</h1>\r\n" + //
-                    "        <form action=\"/hellopost\">\r\n" + //
-                    "            <label for=\"postname\">Name:</label><br>\r\n" + //
-                    "            <input type=\"text\" id=\"postname\" name=\"name\" value=\"John\"><br><br>\r\n" + //
-                    "            <input type=\"button\" value=\"Submit\" onclick=\"loadPostMsg(postname)\">\r\n" + //
-                    "        </form>\r\n" + //
-                    "        \r\n" + //
-                    "        <div id=\"postrespmsg\"></div>\r\n" + //
-                    "        \r\n" + //
-                    "        <script>\r\n" + //
-                    "            function loadPostMsg(name){\r\n" + //
-                    "                let url = \"/hellopost?name=\" + name.value;\r\n" + //
-                    "\r\n" + //
-                    "                fetch (url, {method: 'POST'})\r\n" + //
-                    "                    .then(x => x.text())\r\n" + //
-                    "                    .then(y => document.getElementById(\"postrespmsg\").innerHTML = y);\r\n" + //
-                    "            }\r\n" + //
-                    "        </script>\r\n" + //
-                    "    </body>\r\n" + //
-                    "</html>" + inputLine;
+            System.out.print(path);
+            if(path.equals("/cliente") || "/favicon.ico" == path){
+                outputLine = "HTTP/1.1 200 OK"
+                        + "Content-Type:text/html; charset=ISO-8859-1\r\n"
+                        + "\r\n"
+                        + "<!DOCTYPE html>\r\n" + //
+                        "<html>\r\n" + //
+                        "    <head>\r\n" + //
+                        "        <title>Form Example</title>\r\n" + //
+                        "        <meta charset=\"UTF-8\">\r\n" + //
+                        "        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n" + //
+                        "    </head>\r\n" + //
+                        "    <body>\r\n" + //
+                        "        <h1>Form with GET</h1>\r\n" + //
+                        "        <form action=\"/cliente\">\r\n" + //
+                        "            <label for=\"name\">Name:</label><br>\r\n" + //
+                        "            <input type=\"text\" id=\"name\" name=\"name\" value=\"John\"><br><br>\r\n" + //
+                        "            <input type=\"button\" value=\"Submit\" onclick=\"loadGetMsg()\">\r\n" + //
+                        "        </form> \r\n" + //
+                        "        <div id=\"getrespmsg\"></div>\r\n" + //
+                        "\r\n" + //
+                        "        <script>\r\n" + //
+                        "            function loadGetMsg() {\r\n" + //
+                        "                let nameVar = document.getElementById(\"name\").value;\r\n" + //
+                        "                const xhttp = new XMLHttpRequest();\r\n" + //
+                        "                xhttp.onload = function() {\r\n" + //
+                        "                    document.getElementById(\"getrespmsg\").innerHTML =\r\n" + //
+                        "                    this.responseText;\r\n" + //
+                        "                }\r\n" + //
+                        "                xhttp.open(\"GET\", \"/cliente?name=\"+nameVar);\r\n" + //
+                        "                xhttp.send();\r\n" + //
+                        "            }\r\n" + //
+                        "        </script>\r\n" + //
+                        "\r\n" + //
+                        "    </body>\r\n" + //
+                        "</html>" + inputLine;
                 out.println(outputLine);
                 hasprint = true;
+            }else{
+                outputLine = 
+                "HTTP/1.1 200 OK"
+                + "Content-Type:text/html; charset=ISO-8859-1\r\n"
+                + "\r\n"
+                + "<!DOCTYPE html>\r\n" + //
+                "<html>\r\n" + //
+                " <head>\r\n" + //
+                "        <title>Error de direccion</title>\r\n" + //
+                "        <meta charset=\"UTF-8\">\r\n" + //
+                "        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n" + //
+                "    </head>\r\n"   +
+                "   <body>\r\n" + //
+                "        <h1>ERROR EN EL PATH, COLOCAR /cliente</h1>\r\n" + 
+                "</body>\r\n" + inputLine;
+                out.println(outputLine);
             }
             if(hasprint){
-                String inline = getJson(request);
-                System.out.println(inline);
+                String inline = "";
+                if(cache.containsKey(request)){
+                    inline = cache.get(request);
+                }else{
+                    inline = getJson(request);
+                }
+                cache.put(request, inline);
                 out.println(inline);
             }
             out.flush();
@@ -130,6 +138,7 @@ public class HttpServer {
     private static String getQuery(String text){
         String[] deco1 = text.split(" ");
         String[] deco2 = deco1[1].split("\\?");
+        System.out.println(Arrays.toString(deco2));
         if(deco2.length >=2){
             String[] deco3 = deco2[1].split("\\#");
             return deco3[0];
@@ -138,6 +147,7 @@ public class HttpServer {
         }
     }
 
+
     /**
      * Permite recibir el Json que se esta buscando
      * @param request
@@ -145,29 +155,40 @@ public class HttpServer {
      */
     private static String getJson(String request){
         String[] requests = request.split("=");
-        String defurl = url + requests[1]+key;
-        System.out.println(defurl);
         String res ="";
-        try{
-            URL api = new URL(defurl);
-            HttpURLConnection connection = (HttpURLConnection) api.openConnection();
-            connection.setRequestMethod("GET");
-            connection.connect();
-            int responsecode = connection.getResponseCode();
-            System.out.println("CONNECTION STATUS" + "----->  " + responsecode);
-            String inline = "";
-            Scanner scanner = new Scanner(api.openStream());
-                
-            while (scanner.hasNext()) {
-                inline += scanner.nextLine();
+        if(requests.length > 1){
+            String defurl = url + requests[1]+key;
+            System.out.println(defurl);
+            
+            try{
+                URL api = new URL(defurl);
+                HttpURLConnection connection = (HttpURLConnection) api.openConnection();
+                connection.setRequestMethod("GET");
+                connection.connect();
+                int responsecode = connection.getResponseCode();
+                System.out.println("CONNECTION STATUS" + "----->  " + responsecode);
+                String inline = "";
+                Scanner scanner = new Scanner(api.openStream());
+                    
+                while (scanner.hasNext()) {
+                    inline += scanner.nextLine();
+                }
+                scanner.close();
+                res = inline;     
+            }catch(IOException e){
+                        System.out.println(e.getMessage());
             }
-            scanner.close();
-            res = inline;
-                
-        }catch(IOException e){
-                    System.out.println(e.getMessage());
         }
         return res;
-        
+    }
+
+    private static String getPath(String request){
+        String[] deco1 = request.split(" ");
+        String[] deco2 = deco1[1].split("\\?");
+        if(deco2.length <=1){
+            return deco2[0];
+        }else{
+            return deco2[0];
+        }
     }
 }
